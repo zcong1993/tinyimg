714252b18b789cfd90ce3d8060cf307d7143b86f
zcong1993 <1005997919@qq.com>
2017-11-19 02:52:53 +0800

    tweak build script

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -ev
 NAME=tinyimg
-XC_ARCH=${XC_ARCH:-amd64}
-XC_OS=${XC_OS:-darwin linux windows}
-
 COMMIT=`git describe --always`
 
-
 rm -rf bin/
-gox \
-    -ldflags "-X main.GitCommit=${COMMIT}" \
-    -parallel=-1 \
-    -os="${XC_OS}" \
-    -arch="${XC_ARCH}" \
-    -output "bin/${NAME}-{{.OS}}"
+go build \
+    -ldflags="-X main.GitCommit=${COMMIT}" \
+    ./...
+
+mkdir bin
+mv tinyimg ./bin/
